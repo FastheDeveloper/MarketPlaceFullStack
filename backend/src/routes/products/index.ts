a2dcd10bea0f68a3069601d0ad000b88b00007c6
Farouq Seriki <farouq@basaltx.io>
2024-11-21 16:17:38 +0100

    zod drizzle validation

--- backend/src/routes/products/index.ts
+++ backend/src/routes/products/index.ts
@@ -1,13 +1,15 @@
 import { Router } from "express"
 import { createProduct, deleteProduct, getProductById, listProducts, updateProduct } from "./productsController"
+import { validateData } from "../../middlewares/validationMiddleware"
+import { createProductZodSchema, updateProductZodSchema } from "../../db/productsSchema"
 
-//products endpoints
+
 const router=Router()
 
 router.get('/',listProducts)
 router.get('/:id',getProductById)
-router.post("/",createProduct)
-router.put("/:id",updateProduct)
+router.post("/",validateData(createProductZodSchema),createProduct)
+router.put("/:id",validateData(updateProductZodSchema),updateProduct)
 router.delete('/:id',deleteProduct)
 
 export default router; 
