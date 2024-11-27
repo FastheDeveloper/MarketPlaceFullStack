367af313d9753b4fc09f0670f29d6cb4212524da
Farouq Seriki <farouq@basaltx.io>
2024-11-27 09:19:26 +0100

    gluestack setup

--- mobile-app/src/app/index.tsx
+++ mobile-app/src/app/index.tsx
@@ -1,15 +1,19 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import products from "../../assets/products.json";
 import ProductListItem from "../component/ProductList";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <FlatList
+      {/* <FlatList
         data={products}
         keyExtractor={(item) => String(item.id)}
         renderItem={({ item }) => <ProductListItem item={item} />}
-      />
+      /> */}
+      <Button variant="outline">
+        <ButtonText>PRESS ME</ButtonText>
+      </Button>
     </View>
   );
 }
