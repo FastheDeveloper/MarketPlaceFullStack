1721ff5b60e0795176f62a92cbeb295e552510bb
Farouq Seriki <farouq@basaltx.io>
2024-11-27 09:03:18 +0100

    reuseable product list base structure

--- mobile-app/src/app/index.tsx
+++ mobile-app/src/app/index.tsx
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import products from "../../assets/products.json";
+import ProductListItem from "../component/ProductList";
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <ProductListItem item={item} />}
+      />
     </View>
   );
 }
@@ -14,15 +17,9 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     padding: 24,
   },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
+
   title: {
     fontSize: 64,
     fontWeight: "bold",
---
+++ mobile-app/src/component/ProductList.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function ProductListItem({ item }: any) {
+  return <Text>{item.name}</Text>;
+}
